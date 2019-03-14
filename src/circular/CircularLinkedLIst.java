f1d88e3d9b8dd91454f990a0e982b05564d82490
hsdamak <hmsi13rcb@gmail.com>
2019-03-14 08:33:22 +0530

    Node class created, two constructor of Node class are inserted in class:Node(data), Node(data,node)

--- src/circular/CircularLinkedLIst.java
+++ src/circular/CircularLinkedLIst.java
@@ -1,7 +1,17 @@
 package circular;
 
 public class CircularLinkedLIst {
+    public static class Node{
+        private int data;
+        private Node node;
 
+        public Node(int data) {
+            this.data = data;
+        }
 
+        public Node(int data, Node node) {
+            this.data = data;
+            this.node = node;
+        }
     }
 }
