9f3427a7a7929ab86f0e82e544780916ae58a306
hsdamak <hmsi13rcb@gmail.com>
2019-03-14 08:35:19 +0530

    getter and setter methods for data and next node

--- src/circular/CircularLinkedLIst.java
+++ src/circular/CircularLinkedLIst.java
@@ -21,5 +21,13 @@ public class CircularLinkedLIst {
         public Node getNext() {
             return next;
         }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
     }
 }
